3b6ee538ac324dafa69839102ec985ba09ce6bad
Brian Chiang <brianchiang1988@icloud.com>
2019-11-20 22:00:50 +0800

    Refine two_sum.py

--- No_1_Two Sum/two_sum.py
+++ No_1_Two Sum/two_sum.py
@@ -1,4 +1,6 @@
 '''
+Description:
+
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -9,6 +11,7 @@ Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
+
 '''
 
 
