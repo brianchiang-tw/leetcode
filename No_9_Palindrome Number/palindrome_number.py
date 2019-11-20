37d4a63b50abd7d4668f7e0d940a785802f52fad
Brian Chiang <brianchiang1988@icloud.com>
2019-11-21 00:48:43 +0800

    Add No_9: Palindrome Number

---
+++ No_9_Palindrome Number/palindrome_number.py
@@ -0,0 +1,63 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        str_num = str(x)
+        #print( str_num )
+        return self.checkPalindrome( str_num )
+        
+    def checkPalindrome(self, s):
+        
+        # a palindrome is always symmetric
+        # this in-order string = reversed order string
+        return s == s[::-1]
+    
+    
+'''
+
+Description:
+
+Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+Follow up:
+
+Coud you solve it without converting the integer to a string?
+
+'''
+
+
+def main():
+    import sys
+    import io
+    def readlines():
+        for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
+            yield line.strip('\n')
+
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            x = int(line);
+            
+            ret = Solution().isPalindrome(x)
+
+            out = (ret);
+            print(out)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
