99aaf76e4e624d8d7cab78ec6c426bacff29b303
Brian Chiang <brianchiang1988@icloud.com>
2019-11-27 23:43:45 +0800

    Add enhanced method to No 454_4Sum II

--- No 454_4Sum II aka Four Sum Part II/4_sum_aka_four_sum_part_ii_enhance.py
+++ No 454_4Sum II aka Four Sum Part II/4_sum_aka_four_sum_part_ii_enhance.py
@@ -1,19 +1,27 @@
 '''
+
 Description:
+
 Given four lists A, B, C, D of integer values, compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+
 To make problem a bit easier, all A, B, C, D have same length of N where 0 ≤ N ≤ 500. All integers are in the range of -228 to 228 - 1 and the result is guaranteed to be at most 231 - 1.
+
 Example:
+
 Input:
 A = [ 1, 2]
 B = [-2,-1]
 C = [-1, 2]
 D = [ 0, 2]
+
 Output:
 2
+
 Explanation:
 The two tuples are:
 1. (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
 2. (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+
 '''
 
 
@@ -40,21 +48,36 @@ class Solution:
                 two_sum = a + b
                 
                 # update the method count of two_sum in terms of hashing
-                # key: value
-                # value: total count of pair where pair sum = value
+                # key: two_sum
+                # value: total count of pair where pair sum = two_sum
                 value_hash[ two_sum ] = value_hash.get( two_sum, 0) + 1
         
         
         
+        hash_for_c = dict()
+        # key: element value
+        # value : occurrence of element value
+        for c in C:
+            hash_for_c[c] = C.count( c )
+
+        hash_for_d = dict()
+        # key: element value
+        # value : occurrence of element value
+        for d in D:
+            hash_for_d[d] = D.count( d )
+
+
+
         count_of_zero_4_sum = 0
                 
-        for c in C:
-            for d in D:
+        for c, c_occ in hash_for_c.items():
+            for d, d_occ in hash_for_d.items():
                 
                 dual = 0 - c - d
                 
-                # accumulate the count of four sum where four sum = 0
-                count_of_zero_4_sum += value_hash.get( dual, 0)
+                if dual in value_hash:
+                    # accumulate the count of four sum where four sum = 0
+                    count_of_zero_4_sum += value_hash[dual] * c_occ * d_occ
         
         
         return count_of_zero_4_sum
