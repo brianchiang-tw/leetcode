2e9a7edb4e3f0b4239020eec37da95fe6647d4b6
Brian Chiang <brianchiang1988@icloud.com>
2020-01-06 21:59:16 +0800

    Update Material to No_0113

--- No_0113_Path Sum II/path_sum_ii_dfs.py
+++ No_0113_Path Sum II/path_sum_ii_dfs.py
@@ -1,9 +1,39 @@
+'''
+
+Description:
+
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \    / \
+7    2  5   1
+Return:
+
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+
+'''
+
+
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def pathSum(self, root, s): 
@@ -21,4 +51,52 @@ class Solution:
                 stack.append((curr.left, ls+[curr.left.val]))
         return res
 
-        
+
+
+# n : the number of nodes in binary search tree
+
+## Time Complexity: O( n )
+#
+# The overhead in time is the DFS traversal of whole binary tree, which is of O( n ).
+
+## Space Complexity: O( n )
+#
+# The overhead in space is the storage for output list, which is of O( n ).
+
+
+
+def test_bench():
+
+    root = TreeNode( 5 )
+
+    root.left = TreeNode( 4 )
+    root.right = TreeNode( 8 )
+
+    root.left.left = TreeNode( 11 )
+
+    root.right.left = TreeNode( 13 )
+    root.right.right = TreeNode( 4 )
+
+    root.left.left.left = TreeNode( 7 )
+    root.left.left.right = TreeNode( 2 )
+
+    root.right.right.left = TreeNode( 5 )
+    root.right.right.right = TreeNode( 1 )
+
+    path_of_sum = Solution().pathSum( root, s = 22 )
+
+    # expected output:
+    '''
+    [[5, 4, 11, 2], [5, 8, 4, 5]]
+    '''
+
+
+    print( path_of_sum )
+
+    return 
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
--- No_0113_Path Sum II/path_sum_ii_naive.py
+++ No_0113_Path Sum II/path_sum_ii_naive.py
@@ -1,9 +1,39 @@
+'''
+
+Description:
+
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \    / \
+7    2  5   1
+Return:
+
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+
+'''
+
+
+
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def check_path_sum( self, node: TreeNode, sum, path: List[int]):
