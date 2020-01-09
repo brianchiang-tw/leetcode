ac90ad9d0a4d80fd1c65a5928242cc2a32a9bcbb
Brian Chiang <brianchiang1988@icloud.com>
2020-01-09 20:00:02 +0800

    Revise the test_bench to No_0811

--- No_0811_ Subdomain Visit Count/subdomain_visit_count_by_dictionary.py
+++ No_0811_ Subdomain Visit Count/subdomain_visit_count_by_dictionary.py
@@ -108,6 +108,12 @@ def test_bench():
                     ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]
                 ]
 
+    # expected output:
+    '''
+    ['9001 discuss.leetcode.com', '9001 leetcode.com', '9001 com']
+    ['900 google.mail.com', '901 mail.com', '951 com', '50 yahoo.com', '1 intel.mail.com', '5 wiki.org', '5 org']
+    '''
+
     for test_list in test_data:
         print( Solution().subdomainVisits( test_list ) )
     
