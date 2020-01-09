89741621eb434ba0e92cceff3d3c1fd1e65f5e58
Brian Chiang <brianchiang1988@icloud.com>
2020-01-09 22:28:31 +0800

    Add No_1051: Height Checker

---
+++ No_1051_Height Checker/height_checker_by_sort.py
@@ -0,0 +1,78 @@
+'''
+
+Description:
+
+Students are asked to stand in non-decreasing order of heights for an annual photo.
+
+Return the minimum number of students not standing in the right positions.  (This is the number of students that must move in order for all students to be standing in non-decreasing order of height.)
+
+ 
+
+Example 1:
+
+Input: [1,1,4,2,1,3]
+Output: 3
+Explanation: 
+Students with heights 4, 3 and the last 1 are not standing in the right positions.
+ 
+
+Note:
+
+1 <= heights.length <= 100
+1 <= heights[i] <= 100
+
+'''
+
+
+
+from typing import List
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        
+        well_order = sorted( heights )
+        
+        out_of_order = 0
+        for i in range( len(heights) ):
+            
+            if well_order[i] != heights[i]:
+                out_of_order += 1
+                
+        return out_of_order
+        
+
+
+## Time Complexity: O( n log n ) on average case, O( n ^ 2) on worst case
+#
+# The overhead in time is the cost of sorting, which is O( n log n ) on average case, O( n ^ 2) on worst case
+
+## Space Complexity: O( n )
+#
+# The overhead in space is the storage for list, well_order, which is of O( n )
+
+
+
+def test_bench():
+
+    test_data = [ 
+                    [1,1,4,2,1,3],
+                    [1,2,5,7,9,6]
+                ]
+
+    # expected output:
+    '''
+    3
+    5
+    '''
+
+    for sequence in test_data:
+
+        print( Solution().heightChecker(sequence) )
+    
+    return 
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
+
