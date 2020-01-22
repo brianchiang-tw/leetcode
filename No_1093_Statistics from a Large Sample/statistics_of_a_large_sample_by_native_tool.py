031e9defd99ba4e1bb02eba88177aac59c6142df
Brian Chiang <brianchiang1988@icloud.com>
2020-01-22 19:07:55 +0800

    Add No_1093: Statistics from a Large Sample

---
+++ No_1093_Statistics from a Large Sample/statistics_of_a_large_sample_by_native_tool.py
@@ -0,0 +1,102 @@
+'''
+
+Description:
+
+We sampled integers between 0 and 255, and stored the results in an array count:  count[k] is the number of integers we sampled equal to k.
+
+Return the minimum, maximum, mean, median, and mode of the sample respectively, as an array of floating point numbers.  The mode is guaranteed to be unique.
+
+(Recall that the median of a sample is:
+
+The middle element, if the elements of the sample were sorted and the number of elements is odd;
+The average of the middle two elements, if the elements of the sample were sorted and the number of elements is even.)
+ 
+
+Example 1:
+
+Input: count = [0,1,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+Output: [1.00000,3.00000,2.37500,2.50000,3.00000]
+
+
+
+Example 2:
+
+Input: count = [0,4,3,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+Output: [1.00000,4.00000,2.18182,2.00000,1.00000]
+ 
+
+Constraints:
+
+count.length == 256
+1 <= sum(count) <= 10^9
+The mode of the sample that count represents is unique.
+Answers within 10^-5 of the true value will be accepted as correct.
+
+'''
+
+
+
+#from bisect import bisect_left, bisect_right, bisect
+from bisect import bisect
+from typing import List
+class Solution:
+    def sampleStats(self, count: List[int]) -> List[float]:
+        
+        counter_of_numbers = sum( count )
+        
+        minimum = next( i for i in range(256) if count[i] ) * 1.0
+        maximum = next( i for i in reversed(range(256)) if count[i] ) * 1.0
+        
+        mean_val = sum( occurrence * num for num, occurrence in enumerate(count) ) * 1.0 / counter_of_numbers
+        
+        mode_val = count.index( max(count) ) * 1.0
+        
+        # accumulate occurrence
+        for i in range(255):
+            count[i+1] += count[i]
+            
+        median_low = bisect( count, (counter_of_numbers-1)//2 )
+        median_high = bisect( count, counter_of_numbers//2 )
+        median_val = ( median_low + median_high ) / 2.0
+        
+        return [ minimum, maximum, mean_val, median_val, mode_val ]
+
+
+
+# n : the length of input list, count.
+
+## Time Complexity: O( n )
+#
+# The overhead in time is the loop length and generator length, both of them are of O( n ).
+
+## Space Complexity: O( 1 )
+#
+# The overhead in space is the looping index and temporarily variable for computation, which is of O( 1 ).
+
+
+def test_bench():
+
+    test_data = [
+                    [0,1,3,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                    [0,4,3,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+                ]
+
+    # expected output:
+    '''
+    [1.0, 3.0, 2.375, 2.5, 3.0]
+    [1.0, 4.0, 2.1818181818181817, 2.0, 1.0]    
+    '''
+
+
+    for sequence in test_data:
+
+        print( Solution().sampleStats(sequence) )
+
+    return 
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
