72dba875983b0cec2f939e5d89536f4417da2697
Brian Chiang <brianchiang1988@icloud.com>
2020-02-05 11:51:37 +0800

    Add No_1071: Greatest Common Divisor

---
+++ No_1071_Greatest Common Divisor of Strings/greatest_common_divisor_by_gcd_length.py
@@ -0,0 +1,90 @@
+'''
+
+Description:
+
+For strings S and T, we say "T divides S" if and only if S = T + ... + T  (T concatenated with itself 1 or more times)
+
+Return the largest string X such that X divides str1 and X divides str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+
+Note:
+
+1 <= str1.length <= 1000
+1 <= str2.length <= 1000
+str1[i] and str2[i] are English uppercase letters.
+
+'''
+
+from math import gcd
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        if ( str1 + str2 ) != ( str2 + str1 ):
+            # if str1 and str2 has no common factor, then reject
+            return ''
+        elif str1 == str2:
+            # if str1 and str2 are perfect match, then we find gcd of strings
+            return str1
+        else:
+            # if str1 =\= str2, then solve it by recursive with length_by_gcd
+            length_by_gcd = gcd( len(str1), len(str2) )
+            return self.gcdOfStrings( str1[:length_by_gcd], str2[:length_by_gcd] )
+
+
+
+# n : the length of max( str1, str2)
+
+## Time Complexity: O( log n )
+#
+# The overhead in time is the cost of gcd, which is of O( log n )
+
+
+## Space Compleixty: O( n )
+#
+# The overhead in space is the storage for string concatenation, which is of O( n )
+
+
+
+def test_bench():
+
+    test_data = [
+                    ("ABCABC", "ABC"),
+                    ("ABABAB", "ABAB"),
+                    ("LEET", "CODE")
+                ]
+
+    # expected output:
+    # note: the third output is empty stirng ''
+    '''
+    ABC
+    AB
+
+    '''
+
+    for string_1, string_2 in test_data:
+
+        print( Solution().gcdOfStrings(string_1, string_2) )
+    
+    return 
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
