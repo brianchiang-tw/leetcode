0636ed377f9f06e684520ebebbd7228b364af094
Brian Chiang <brianchiang1988@icloud.com>
2020-02-05 16:00:34 +0800

    Add another implementation to No_1315

---
+++ No_1315_Sum of Nodes with Even-Valued Grandparent/sum_of_nodes_with_even_valued_grandparent_by_dfs_on_stack.py
@@ -0,0 +1,112 @@
+'''
+
+Description:
+
+Given a binary tree, return the sum of values of nodes with even-valued grandparent.  (A grandparent of a node is the parent of its parent, if it exists.)
+
+If there are no nodes with an even-valued grandparent, return 0.
+
+ 
+
+Example 1:
+
+
+
+Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+Output: 18
+Explanation: The red nodes are the nodes with even-value grandparent while the blue nodes are the even-value grandparents.
+ 
+
+Constraints:
+
+The number of nodes in the tree is between 1 and 10^4.
+The value of nodes is between 1 and 100.
+
+'''
+
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        
+        if not root:
+            # empty tree
+            return 0
+        
+        # initialization:
+        summation = 0
+        
+        # root node, -1 for init, -1 for init
+        dfs_stack = [ (root, -1, -1) ]
+        
+        
+        # pre-order DFS traveral
+        while dfs_stack:
+            
+            node, parent, grandparent = dfs_stack.pop()
+            
+            if grandparent & 1 == 0:
+                summation += node.val
+            
+            if node.right:
+                dfs_stack.append( (node.right, node.val, parent) )
+            
+            if node.left:
+                dfs_stack.append( (node.left, node.val, parent) )
+            
+        
+        return summation
+
+
+
+# n : the number of nodes in binary tree
+
+## Time Complexity: O( n )
+#
+# The overhead in time is the cost of pre-order DFS traversal, which is if O( n ).
+
+## Space Complexity: O( n )
+#
+# The overhead in space is the storage for dfs_stack, which is of O( n ).
+
+
+
+def test_bench():
+
+    root = TreeNode(6)
+
+    root.left = TreeNode(7)
+    root.right = TreeNode(8)
+
+    root.left.left = TreeNode(2)
+    root.left.right = TreeNode(7)
+
+    root.right.left = TreeNode(1)
+    root.right.right = TreeNode(3)
+
+    root.left.left.left = TreeNode(9)
+    root.left.right.left = TreeNode(1)
+    root.left.right.right = TreeNode(4)
+    root.right.right.right = TreeNode(5)
+
+    # expected output:
+    '''
+    18
+    '''
+
+    print( Solution().sumEvenGrandparent(root) )
+
+    return 
+
+
+
+if __name__ == '__main__':
+    
+    test_bench()
