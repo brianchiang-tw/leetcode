ecde8ecb7b0109ab9274fd907c248cee00d9dfbb
Brian Chiang <brianchiang1988@icloud.com>
2020-02-08 11:12:11 +0800

    Add No_1302: Deepest Leaves Sum

---
+++ No_1302_Deepest Leaves Sum/deeoest_leaves_sum_by_level_order_traversal.py
@@ -0,0 +1,107 @@
+'''
+
+Description:
+
+Given a binary tree, return the sum of values of its deepest leaves.
+ 
+
+Example 1:
+
+
+
+Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+Output: 15
+ 
+
+Constraints:
+
+The number of nodes in the tree is between 1 and 10^4.
+The value of nodes is between 1 and 100.
+
+'''
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+
+class Solution:
+    def deepestLeavesSum(self, root: TreeNode) -> int:
+        
+        prev_level = []
+        traversal_queue = [ root ]
+        
+        # Level-order-traversal
+        while traversal_queue:
+            
+            next_level_queue = []
+            
+            for cur_level_node in traversal_queue:
+                
+                if cur_level_node.left:
+                    next_level_queue.append( cur_level_node.left )    
+                
+                if cur_level_node.right:
+                    next_level_queue.append( cur_level_node.right )
+            
+            # update previous level as current traversal queue 
+            # update current traversal queue as next level queue
+            prev_level, traversal_queue = traversal_queue, next_level_queue
+        
+        
+        # When the level-order-traversal is completed,
+        # prev_level contains those nodes in deepest level
+        return sum( node.val for node in prev_level if node )
+                
+
+
+# n : the number of nodes
+
+## Time Complexity: O(n)
+#
+# The overhead in time is the cost of level-order traversal, which is of O( n )
+
+## Space Complexity: O(n)
+#
+# The overhead in space is the storage for traversal_queue, which is of O( n )
+
+
+
+def test_bench():
+
+    node_1 = TreeNode(1)
+    node_2 = TreeNode(2)
+    node_3 = TreeNode(3)
+    node_4 = TreeNode(4)
+    node_5 = TreeNode(5)
+    node_6 = TreeNode(6)
+    node_7 = TreeNode(7)
+    node_8 = TreeNode(8)
+    
+    node_4.left = node_7
+
+    node_2.left = node_4
+    node_2.right = node_5
+
+    node_6.right = node_8
+    node_3.right = node_6
+    
+    node_1.left = node_2
+    node_1.right = node_3
+
+    root = node_1
+
+    print( Solution().deepestLeavesSum(root) )
+
+    return
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
