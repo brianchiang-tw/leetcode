49ebd09ac1c9774be6600f7fffd9a45e48352e18
Brian Chiang <brianchiang1988@icloud.com>
2020-03-01 20:20:10 +0800

    Add No_0653: Two Sum IV - Input is a BST

---
+++ No_0653_Two Sum IV - Input is a BST/by_preorder_and_set.py
@@ -0,0 +1,122 @@
+'''
+
+Description:
+
+Given a Binary Search Tree and a target number, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+
+
+Example 1:
+
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 9
+
+Output: True
+ 
+
+
+Example 2:
+
+Input: 
+    5
+   / \
+  3   6
+ / \   \
+2   4   7
+
+Target = 28
+
+Output: False
+
+'''
+
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def findTarget(self, root: TreeNode, k: int) -> bool:
+        
+        num_set = set()
+        
+        def helper( node: TreeNode, k: int)->bool:
+            
+            nonlocal num_set
+            
+            if not node:
+                return False
+            
+            if node.val in num_set:
+                return True
+            
+            num_set.add( k - node.val )
+            
+            return helper( node.left, k) or helper( node.right, k )
+        # --------------------------------------------------------------
+        
+        return helper( root, k) if root else False
+                   
+
+
+# n : the number of nodes in binary search tree
+
+## Time Complexity: O( n )
+#
+# The overhead in time is the cost of DFS traversal, which is of O( n )
+
+## Space Complexity: O( n )
+#
+# The overhead in space is the storage for set, num_set, which is of O( n )
+
+
+
+def test_bench():
+
+    ## Test_case_#1
+    root_1 = TreeNode(5)
+
+    root_1.left = TreeNode(3)
+    root_1.right = TreeNode(6)
+
+    root_1.left.left = TreeNode(2)
+    root_1.left.right = TreeNode(4)
+    root_1.right.right = TreeNode(7)
+    # expected output:
+    '''
+    True
+    '''
+    print( Solution().findTarget(root = root_1, k = 9) )
+
+
+
+    ## Test_case_#2
+    root_1 = TreeNode(5)
+
+    root_1.left = TreeNode(3)
+    root_1.right = TreeNode(6)
+
+    root_1.left.left = TreeNode(2)
+    root_1.left.right = TreeNode(4)
+    root_1.right.right = TreeNode(7)
+    # expected output:
+    '''
+    False
+    '''
+    print( Solution().findTarget(root = root_1, k = 28) )
+
+
+if __name__ == '__main__':
+
+    test_bench()
+                    
