fbed708db676bf8b0d9ae33f5e533a0a04c9e630
Brian Chiang <brianchiang1988@icloud.com>
2020-03-01 12:46:24 +0800

    Add No_0049: Group Anagrams

---
+++ No_0049_Group Anagrams/group_anagrams_by_dict_and_sort.py
@@ -0,0 +1,81 @@
+'''
+
+Description:
+
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+
+'''
+
+
+
+from collections import Counter
+from collections import defaultdict
+from typing import List
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        anagram_dict = defaultdict( list )
+        
+        
+        for s in strs:
+            
+            ordered_s = ''.join(sorted(s))
+            
+            anagram_dict[ordered_s].append( s )
+                
+        return [ anagram_dict[key_string] for key_string in anagram_dict ]
+
+
+
+# n : the number of strings in input strs.
+# k : the average character length of strings
+
+## Time Complexity: O( n k log k)
+#
+# The overhead in time is the for loop, iterating on strs, which is of O( n ),
+# and the cost of sorted(s), which is of O( k log k ).
+# It takes O( n k log k ) in total.
+
+## Space Complexity: O( n )
+#
+# The overhead in space is the storage for dictionary, anagram_dict, which is of O( n )
+                
+
+from collections import namedtuple
+TestEntry = namedtuple('TestEntry', 'input_string')
+def test_bench():
+
+    test_data = [
+                    TestEntry( input_string = ["eat", "tea", "tan", "ate", "nat", "bat"] )
+                ]
+
+    # expected output:
+    '''
+    [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+    '''
+
+    for t in test_data:
+
+        print( Solution().groupAnagrams( t.input_string) )
+
+    return
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
