1ee48ab66c70f65340a2480e9ed9a4367323f25a
Brian Chiang <brianchiang1988@icloud.com>
2020-04-12 13:11:32 +0800

    Add No_1408: String Matching in an Array

---
+++ No_1408_String Matching in an Array/by_generator_expression.py
@@ -0,0 +1,98 @@
+'''
+
+Description:
+
+Given an array of string words. Return all strings in words which is substring of another word in any order. 
+
+String words[i] is substring of words[j], if can be obtained removing some characters to left and/or right side of words[j].
+
+ 
+
+Example 1:
+
+Input: words = ["mass","as","hero","superhero"]
+Output: ["as","hero"]
+Explanation: "as" is substring of "mass" and "hero" is substring of "superhero".
+["hero","as"] is also a valid answer.
+
+
+
+Example 2:
+
+Input: words = ["leetcode","et","code"]
+Output: ["et","code"]
+Explanation: "et", "code" are substring of "leetcode".
+Example 3:
+
+Input: words = ["blue","green","bu"]
+Output: []
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 30
+words[i] contains only lowercase English letters.
+It's guaranteed that words[i] will be unique.
+
+'''
+
+
+
+from typing import List
+
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        
+        size = len( words )
+        substr = set( words[i] for i in range(size) for j in range(size) if i != j and words[i] in words[j] )
+        
+        return [ *substr ]
+
+
+
+# n : the length of input list, words.
+# k : the average character length of words
+
+## Time Complexity: O( n^2 * k)
+#
+# The overhead in time is the cost of nested loop in generator expression, which is of O( n^2 )
+# And the substring checking takes O( k ).
+#
+# It takes O( n^2 * k ) in total.
+
+## Space Complexity: O( n )
+#
+# The overhead in space is the storage for output list, which is of O( n )
+
+
+
+from collections import namedtuple
+TestEntry = namedtuple('TestEntry', 'words')
+def test_bench():
+
+    test_data = [
+                    TestEntry( words = ["mass","as","hero","superhero"] ),
+                    TestEntry( words = ["leetcode","et","code"] ),
+                    TestEntry( words = ["blue","green","bu"] ),
+                ]
+
+
+    # expected output:
+    '''
+    ['as', 'hero']
+    ['et', 'code']
+    []
+    '''
+
+    for t in test_data:
+
+        print( Solution().stringMatching( words = t.words ) )
+
+    return
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
