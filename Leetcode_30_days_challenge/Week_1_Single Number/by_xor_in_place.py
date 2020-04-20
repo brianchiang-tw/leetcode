903a1113f84b63ff38cbacebf97b8508b350b9d8
Brian Chiang <brianchiang1988@icloud.com>
2020-04-20 15:26:08 +0800

    Add Leetcode 30 days challegne: Week_1 Single Number

---
+++ Leetcode_30_days_challenge/Week_1_Single Number/by_xor_in_place.py
@@ -0,0 +1,80 @@
+'''
+
+Description:
+
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+
+
+
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+
+
+'''
+
+
+
+from typing import List
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        
+        # Utilize the property of XOR, A ⊕ A = 0, to cancel those elements which appeared twice.
+
+        for idx in range( 1, len(nums) ):
+            nums[0] ^= nums[idx]
+            
+        return nums[0]
+
+
+
+# n : the length of input list, nums.
+
+## Time Complexity: O( n )
+#
+# The overhead in time is the for loop, which is of O( n ).
+
+## Space Complexity: O( 1 )
+#
+# The overhead in space is the storage for loop index, which are of O( 1 )
+
+
+
+from collections import namedtuple
+TestEntry = namedtuple('TestEntry', 'sequence')
+def test_bench():
+
+    test_data = [
+                    TestEntry( sequence = [2,2,1] ),
+                    TestEntry( sequence = [4,1,2,1,2] ),
+                    TestEntry( sequence = [1,1,3,2,3,5,4,4,5] ),
+                ]
+
+    # expected output:
+    '''
+    1
+    4
+    2
+    '''
+
+    for t in test_data:
+
+        print( Solution().singleNumber( nums = t.sequence) )
+    
+    return
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
