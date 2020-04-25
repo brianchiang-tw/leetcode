8cec1ae7a0d140aaf7f881e74d2a10fbe64d7125
Brian Chiang <brianchiang1988@icloud.com>
2020-04-25 12:03:28 +0800

    Refine implementation to No_0416 LRU Cache

--- No_0146_LRU Cache/by_ordered_dict.py
+++ No_0146_LRU Cache/by_ordered_dict.py
@@ -100,9 +100,9 @@ def test_bench():
     cache.put(1, 1)
     cache.put(2, 2)
     print( cache.get(1) )   # returns 1
-    cache.put(3, 3);        # evicts key 2
+    cache.put(3, 3)         # evicts key 2
     print( cache.get(2) )   # returns -1 (not found)
-    cache.put(4, 4);        # evicts key 1
+    cache.put(4, 4)         # evicts key 1
     print( cache.get(1) )   # returns -1 (not found)
     print( cache.get(3) )   # returns 3
     print( cache.get(4) )   # returns 4
