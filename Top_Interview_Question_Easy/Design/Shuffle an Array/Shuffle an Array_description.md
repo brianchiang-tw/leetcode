4e4857db8678b2a4bcc1cc186fc598d34a8bb68a
Brian Chiang <brianchiang1988@icloud.com>
2020-05-02 15:22:54 +0800

    Add TIC_Easy: Shuffle an Array

---
+++ Top_Interview_Question_Easy/Design/Shuffle an Array/Shuffle an Array_description.md
@@ -0,0 +1,26 @@
+'''
+
+Description:
+
+Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+
+
+Hint #1  
+The solution expects that we always use the original array to shuffle() else some of the test cases fail. (Credits; @snehasingh31)
+
+'''
---
+++ Top_Interview_Question_Easy/Design/Shuffle an Array/by_random_sample.py
@@ -0,0 +1,93 @@
+'''
+
+Description:
+
+Shuffle a set of numbers without duplicates.
+
+Example:
+
+// Init an array with set 1, 2, and 3.
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// Shuffle the array [1,2,3] and return its result. Any permutation of [1,2,3] must equally likely to be returned.
+solution.shuffle();
+
+// Resets the array back to its original configuration [1,2,3].
+solution.reset();
+
+// Returns the random shuffling of array [1,2,3].
+solution.shuffle();
+
+
+Hint #1  
+The solution expects that we always use the original array to shuffle() else some of the test cases fail. (Credits; @snehasingh31)
+
+'''
+
+
+
+from typing import List
+from random import sample
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        
+        # copy to class member: self.array
+        self.array = [ *nums ]
+        
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.array
+        
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        size = len(self.array)
+        return sample(self.array, size)
+
+
+
+# n : the length of input array
+
+## Time Complexity: O( n )
+#
+# The overhead in time is the cost of shuffle and reset, which are of O( n ).
+
+## Space Complexity: O( n )
+#
+# The overhead in space is the storage for class member, self.array, which is of O( n ).
+
+
+from collections import namedtuple
+TestEntry = namedtuple('TestEntry', 'sequence')
+
+def test_bench():
+
+    t = TestEntry( sequence = [1,2,3] )
+
+    # reference output
+    # this is a challenge about randomness, so any valid permutation of shuffling result is accpeted.
+
+    '''
+    [1, 3, 2]
+    [1, 2, 3]
+    [3, 2, 1]
+    '''
+
+    obj = Solution( t.sequence )
+    print( obj.shuffle() )
+    print( obj.reset() )
+    print( obj.shuffle() )
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
