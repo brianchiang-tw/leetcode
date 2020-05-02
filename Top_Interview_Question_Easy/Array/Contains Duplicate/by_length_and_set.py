a27c89a6c31beb1e81250d2d6729f8a3c0f5c4f8
Brian Chiang <brianchiang1988@icloud.com>
2020-05-02 13:17:56 +0800

    Add TIQ_Easy: Contains Duplicate

---
+++ Top_Interview_Question_Easy/Array/Contains Duplicate/by_length_and_set.py
@@ -0,0 +1,80 @@
+'''
+
+Description:
+
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+
+
+
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+
+
+
+Example 3:
+
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+'''
+
+
+
+
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        return len(nums) > len( set(nums) )
+
+
+
+# n : the length of input array
+
+## Time Complexity: O( n )
+#
+# The overhead in time is to building the set of n elements and getting the length, which is of O( n )
+
+## Space Complexity: O( n )
+#
+# The overhead in space is the storage for the set of nums, which is of O( n )
+
+
+
+def test_bench():
+
+    test_data = [
+                    [1,2,3,1],
+                    [1,2,3,4],
+                    [1,1,1,3,3,4,3,2,4,2]
+                ]
+
+
+    # expected output:
+    '''
+    True
+    False
+    True
+    '''
+
+    for test_arr in test_data:
+
+        print( Solution().containsDuplicate(test_arr) )
+    
+    return 
+
+
+
+if __name__ == '__main__':
+
+    test_bench()
