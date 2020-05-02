5c6d63bc72a61adf6c7be6fd02c7352f657c9e87
Brian Chiang <brianchiang1988@icloud.com>
2020-05-02 16:25:52 +0800

    Add TIQ_Easy: Valid Sudoku

---
+++ Top_Interview_Question_Easy/Array/Valid Sudoku/by_existence_flag.py
@@ -0,0 +1,160 @@
+'''
+
+Description:
+
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+A partially filled sudoku which is valid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+Example 1:
+
+Input:
+[
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: true
+
+
+
+Example 2:
+
+Input:
+[
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being 
+    modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+The given board contain only digits 1-9 and the character '.'.
+The given board size is always 9x9.
+
+'''
+
+
+
+from typing import List
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        # Row flag for 1~9
+        row_flag = [[False for i in range(9)] for j in range(9)]
+        
+        # Column flag for 1~9
+        col_flag = [[False for i in range(9)] for j in range(9)]
+        
+        # 3x3 sub-block flag for 1~9
+        block_flag = [[False for i in range(9)] for j in range(9)]
+        
+        for i in range(9):
+            for j in range(9):
+                
+                if board[i][j] != '.':
+                    
+                    # get current number
+                    num = int(board[i][j]) - 1
+
+                    # compute index of 3x3 sub-block
+                    k = i//3*3 + j//3
+                    
+                    # Reject if there is number repeated in row, column or 3x3 sub-block
+                    if row_flag[i][num] or col_flag[j][num] or block_flag[k][num]:
+                        return False
+                    
+                    # update row flag, column flag, 3x3 sub-block flagfor current number
+                    row_flag[i][num] = col_flag[j][num] = block_flag[k][num] = True
+                    
+        return True
+
+
+
+# n : the side length of board
+
+## Time Complexity: O( n^2 )
+#
+# The overhead in time is the cost of nested loop, which is of O( n^2 )
+
+## Space Complexity: O( n^2 )
+#
+# The overhead in space is the storage for flag table, 
+
+
+
+
+from collections import namedtuple
+TestEntry = namedtuple('TestEntry', 'board')
+def test_bench():
+
+    test_data = [
+                    TestEntry( board = [
+                                            ["5","3",".",".","7",".",".",".","."],
+                                            ["6",".",".","1","9","5",".",".","."],
+                                            [".","9","8",".",".",".",".","6","."],
+                                            ["8",".",".",".","6",".",".",".","3"],
+                                            ["4",".",".","8",".","3",".",".","1"],
+                                            ["7",".",".",".","2",".",".",".","6"],
+                                            [".","6",".",".",".",".","2","8","."],
+                                            [".",".",".","4","1","9",".",".","5"],
+                                            [".",".",".",".","8",".",".","7","9"]
+                                        ]
+                                ),
+
+                    TestEntry( board = [
+                                            ["8","3",".",".","7",".",".",".","."],
+                                            ["6",".",".","1","9","5",".",".","."],
+                                            [".","9","8",".",".",".",".","6","."],
+                                            ["8",".",".",".","6",".",".",".","3"],
+                                            ["4",".",".","8",".","3",".",".","1"],
+                                            ["7",".",".",".","2",".",".",".","6"],
+                                            [".","6",".",".",".",".","2","8","."],
+                                            [".",".",".","4","1","9",".",".","5"],
+                                            [".",".",".",".","8",".",".","7","9"]
+                                        ]
+                                ),
+                        
+                ]
+
+    # expected output:
+    '''
+    True
+    False
+    '''
+
+
+    for t in test_data:
+
+        print( Solution().isValidSudoku( board = t.board) )                
+
+    return
+
+
+
+if __name__ == '__main__':
+
+    test_bench()    
