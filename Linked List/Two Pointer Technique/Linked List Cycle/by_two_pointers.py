73ea6ebe56d17302578a9a1c3fa8ff999fe7f28f
Brian Chiang <brianchiang1988@icloud.com>
2020-05-04 16:30:21 +0800

    Add test bench and complexity analysis to Linked List Cycle

--- Linked List/Two Pointer Technique/Linked List Cycle/by_two_pointers.py
+++ Linked List/Two Pointer Technique/Linked List Cycle/by_two_pointers.py
@@ -68,4 +68,47 @@ class Solution:
             
             
             
-            
+
+
+
+# N : total number of nodes in linked list
+
+## Time complexity : O( N )
+#
+# The upper bound is decided by the fast runner, 
+# the procedure ends either find a cycle( meets slow runner somewhere) or fetch the end of linked list ( None )
+# Both cases are of O( N )
+
+## Space complexity : O( 1 )
+#
+# The overhead is variable with fixed size for loop pivot fast runner and slow runner, of O( 1 ).
+
+
+
+def test_bench():
+
+
+    #   1 -> 2 -|
+    #   ^       |
+    #   |       |
+    #   ---------
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = head
+
+    # True
+    print( Solution().hasCycle(head) )
+
+
+
+    # 1 -> 2 -> 3 -> None
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+
+    # False
+    print( Solution().hasCycle(head) )
+
+if __name__ == '__main__':
+
+    test_bench()            
