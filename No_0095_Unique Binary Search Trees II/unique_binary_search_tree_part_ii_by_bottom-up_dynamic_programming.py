40a9c0bfd726ca47845e5dbf10ebb517240befec
Brian Chiang <brianchiang1988@icloud.com>
2020-05-15 15:08:03 +0800

    Revise implementation to No_0095

--- No_0095_Unique Binary Search Trees II/unique_binary_search_tree_part_ii_by_bottom-up_dynamic_programming.py
+++ No_0095_Unique Binary Search Trees II/unique_binary_search_tree_part_ii_by_bottom-up_dynamic_programming.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     
