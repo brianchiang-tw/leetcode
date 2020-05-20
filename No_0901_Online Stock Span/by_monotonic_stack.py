bf0960f5601c7762969c9d60c445509ee432443b
Brian Chiang <brianchiang1988@icloud.com>
2020-05-20 16:58:21 +0800

    Add No_0901:Online Stock Span

---
+++ No_0901_Online Stock Span/by_monotonic_stack.py
@@ -0,0 +1,101 @@
+'''
+
+Description:
+
+Write a class StockSpanner which collects daily price quotes for some stock, and returns the span of that stock's price for the current day.
+
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backwards) for which the price of the stock was less than or equal to today's price.
+
+For example, if the price of a stock over the next 7 days were [100, 80, 60, 70, 60, 75, 85], then the stock spans would be [1, 1, 1, 2, 1, 4, 6].
+
+ 
+
+Example 1:
+
+Input: ["StockSpanner","next","next","next","next","next","next","next"], [[],[100],[80],[60],[70],[60],[75],[85]]
+Output: [null,1,1,1,2,1,4,6]
+Explanation: 
+First, S = StockSpanner() is initialized.  Then:
+S.next(100) is called and returns 1,
+S.next(80) is called and returns 1,
+S.next(60) is called and returns 1,
+S.next(70) is called and returns 2,
+S.next(60) is called and returns 1,
+S.next(75) is called and returns 4,
+S.next(85) is called and returns 6.
+
+Note that (for example) S.next(75) returned 4, because the last 4 prices
+(including today's price of 75) were less than or equal to today's price.
+ 
+
+Note:
+
+Calls to StockSpanner.next(int price) will have 1 <= price <= 10^5.
+There will be at most 10000 calls to StockSpanner.next per test case.
+There will be at most 150000 calls to StockSpanner.next across all test cases.
+The total time limit for this problem has been reduced by 75% for C++, and 50% for all other languages.
+
+'''
+
+
+
+class StockSpanner:
+    
+    def __init__(self):
+        
+        # record for stock
+        
+        # first parameter is price quote
+        # second parameter is price span
+        self.monotone_stack = []
+              
+        
+        
+    def next(self, price: int) -> int:
+
+        stack = self.monotone_stack
+        
+        cur_price_quote, cur_price_span = price, 1
+        
+        #while stack and stack[-1].price_quote <= cur_price_quote:
+        while stack and stack[-1][0] <= cur_price_quote:
+            
+            prev_price_quote, prev_price_span = stack.pop()
+            
+            # update current price span with history data in stack
+            cur_price_span += prev_price_span
+        
+        # Update latest price quote and price span
+        stack.append( (cur_price_quote, cur_price_span) )
+        
+        return cur_price_span
+
+
+
+    # n : the number of next() operations
+
+    ## Time Complexity: O( 1 )
+    #
+    # Each element wil be push() at most once and pop() at most once also.
+
+    ## Space Complexity: O( n )
+    #
+    # The overhead in space is the stroage for stack, which is of O( n ).
+
+    
+
+def test_bench():
+
+    S = StockSpanner()
+    print( S.next(100) )    # returns 1
+    print( S.next(80) )     # returns 1
+    print( S.next(60) )     # returns 1
+    print( S.next(70) )     # returns 2
+    print( S.next(60) )     # returns 1
+    print( S.next(75) )     # returns 4
+    print( S.next(85) )     # returns 4
+
+
+if __name__ == '__main__':
+
+    test_bench()        
