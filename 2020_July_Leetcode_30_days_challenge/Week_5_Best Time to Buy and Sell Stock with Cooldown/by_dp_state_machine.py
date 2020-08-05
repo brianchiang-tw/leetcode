047cd7305dbb3844012d7e4d1d4f0525ffca07c3
Brian Chiang <brianchiang1988@icloud.com>
2020-08-05 21:55:53 +0800

    2020 July Leetcode 30 Days challenge: Week_5 Best Time to Buy and Sell Stock with Cooldown

---
+++ 2020_July_Leetcode_30_days_challenge/Week_5_Best Time to Buy and Sell Stock with Cooldown/by_dp_state_machine.py
@@ -0,0 +1,72 @@
+'''
+
+Description:
+
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times) with the following restrictions:
+
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+After you sell your stock, you cannot buy stock on next day. (ie, cooldown 1 day)
+Example:
+
+Input: [1,2,3,0,2]
+Output: 3 
+Explanation: transactions = [buy, sell, cooldown, buy, sell]
+
+'''
+
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+		    # initialization
+        cool_down, sell, hold = 0, 0, -float('inf')
+        
+        for stock_price_of_Day_i in prices:
+            
+            prev_cool_down, prev_sell, prev_hold = cool_down, sell, hold
+            
+            # Max profit of cooldown on Day i comes from either cool down of Day_i-1, or sell out of Day_i-1 and today Day_i is cooling day
+            cool_down = max(prev_cool_down, prev_sell)
+            
+            # Max profit of sell on Day_i comes from hold of Day_i-1 and sell on Day_i
+            sell = prev_hold + stock_price_of_Day_i
+            
+            # Max profit of hold on Day_i comes from either hold of Day_i-1, or cool down on Day_i-1 and buy on Day_i
+            hold = max(prev_hold, prev_cool_down - stock_price_of_Day_i)
+        
+        
+        # The action of final trading day must be either sell or cool down
+        return max(sell, cool_down)
+
+
+
+# n : length of prices
+
+## Time Complexity: O( n )
+#
+# The overhead in time is the cost of loop iteration, which is of O( n ).
+
+## Space Complexity: O(1)
+#
+# The overhead in space is the storage for loop index and temporary variable, which is of O( 1 ).
+
+
+
+import unittest
+
+class Testing(unittest.TestCase):
+
+  def test_case_1(self):
+
+    result = Solution().maxProfit(prices=[1,2,3,0,2])
+    self.assertEqual(result, 3)
+
+
+
+if __name__ == '__main__':
+
+  unittest.main()    
